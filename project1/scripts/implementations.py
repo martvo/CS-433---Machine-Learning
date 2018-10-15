5acd6ac1c2a6a4d2332d5219928b5993b0bcab9a
martvo <martvo@stud.ntnu.no>
2018-10-15 16:08:48 +0200

    fist

--- project1/scripts/implementations.py
+++ project1/scripts/implementations.py
@@ -1,34 +1,81 @@
-import proj1_helpers
+from proj1_helpers import load_csv_data, predict_labels, create_csv_submission
+import numpy as np
 
-def least_squares(y, tx):
-    """calculate the least squares solution."""
-    N = tx.shape[0]
-    w = np.linalg.solve(np.transpose(tx).dot(tx),np.transpose(tx).dot(y))
-    mse = 1/(2*N)*sum((y-tx.dot(w))**2)
-    return (mse, w)
+
+def least_squares(y, tx, w):
+    """ Least squares using the normal equation """
+
+    loss = compute_loss(y, tx, w)
+    grad, error = compute_gradient(y, tx, w)
+    return loss, grad, error
+
+
+def compute_loss(y, tx, w):
+    return 1/(2*tx.shape[0]) * sum((y-tx.dot(w))**2)
+
 
 def compute_gradient(y, tx, w):
     err = y - tx.dot(w)
     grad = -tx.T.dot(err) / len(err)
-    return err, grad
+    return grad, err
+
 
 def least_squares_GD(y, tx, initial_w, max_iters, gamma):
+    """Least squares using Gradient Descent"""
+
     ws = [initial_w]
     losses = []
     w = initial_w
     for n_iter in range(max_iters):
-        grad = compute_gradient(y,tx,w)
-        loss =compute_loss(y,tx,w)
+        # Compute loss, grad and error with the least squares method
+        loss, grad, error = least_squares(y, tx, w)
+
+        # Update w
         w = w - grad*gamma
+
         # store w and loss
         ws.append(w)
         losses.append(loss)
+
         #print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
         #      bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
 
-    return losses, ws
+    return losses[-1], ws[-1]
+
+
+def least_squares_SGD(y, tx, initial_w, max_iters, gamma, batch_size):
+    """Least squares using Stochastic Gradient Descent"""
+
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+    current_y = y
+    current_tx = tx
+    for n_iter in range(max_iters):
+        # Choose subset to use for this iteration, use np.random.permutation
+        # Get a permuted list of the length of the length of tx as this list can be used to choose the same
+        # samples from both tx and y. Which is important to get the right result!
+
+        permuted_list = np.random.permutation(tx.shape[0])
+        current_tx = tx[permuted_list[:batch_size]]
+        current_y = y[permuted_list[:batch_size]]
+
+        # Compute loss, grad and error with the least squares method
+        loss, grad, error = least_squares(current_y, current_tx, w)
+
+        # Update w
+        w = w - grad * gamma
+
+        # store w and loss
+        ws.append(w)
+        losses.append(loss)
+
+    return losses[-1], ws[-1]
+
 
 if __name__ == "__main__":
-    proj1_helpers.load_csv_data("../data/train.csv.zip")
-    print("Her")
+    y_train, x_train, ids_train = load_csv_data("../data/train.csv")
+    y_test, x_test, ids_test = load_csv_data("../data/test.csv")
 
+    # Vi må behandle dataen vi har før vi bruker den
+    # Visualiser data i en notebook, enklere der.. Kan ikke gjøre det nå pga. Anaconda er fucka..
