496d34097120e270179072ab38ee6c0efe6216a5
aalexjo <alexanjohansen@gmail.com>
2018-10-24 16:45:47 +0200

    Was able to create an acceptable submission using ridge regression, lot of testing still to be done

--- project1/scripts/implementations.py
+++ project1/scripts/implementations.py
@@ -1,22 +1,5 @@
-from proj1_helpers import load_csv_data, predict_labels, create_csv_submission
+from proj1_helpers import *
 import numpy as np
-
-
-def least_squares(y, tx):
-    """ Least squares using the normal equation """
-    w = np.linalg.solve(tx.T.dot(tx), tx.T.dot(y))
-    loss = compute_loss(y, tx, w)
-    return loss, w
-
-
-def compute_loss(y, tx, w):
-    return 1/(2*tx.shape[0]) * sum((y-tx.dot(w))**2)
-
-
-def compute_gradient(y, tx, w):
-    err = y - tx.dot(w)
-    grad = -tx.T.dot(err) / len(err)
-    return grad, err
 
 
 def least_squares_GD(y, tx, initial_w, max_iters, gamma):
@@ -27,8 +10,8 @@ def least_squares_GD(y, tx, initial_w, max_iters, gamma):
     w = initial_w
     for n_iter in range(max_iters):
         # Compute loss, grad and error with the least squares method
-        loss, grad, error = least_squares(y, tx, w)
-
+        grad, error = compute_gradient(y, tx, w)
+        loss = calculate_mse(error)
         # Update w
         w = w - grad*gamma
 
@@ -43,38 +26,47 @@ def least_squares_GD(y, tx, initial_w, max_iters, gamma):
 
 
 def least_squares_SGD(y, tx, initial_w, max_iters, gamma, batch_size):
-    """Least squares using Stochastic Gradient Descent"""
-
+    """Stochastic gradient descent algorithm."""
     ws = [initial_w]
     losses = []
     w = initial_w
-    current_y = y
-    current_tx = tx
-    for n_iter in range(max_iters):
-        # Choose subset to use for this iteration, use np.random.permutation
-        # Get a permuted list of the length of the length of tx as this list can be used to choose the same
-        # samples from both tx and y. Which is important to get the right result!
-
-        permuted_list = np.random.permutation(tx.shape[0])
-        current_tx = tx[permuted_list[:batch_size]]
-        current_y = y[permuted_list[:batch_size]]
-
-        # Compute loss, grad and error with the least squares method
-        loss, grad, error = least_squares(current_y, current_tx, w)
-
-        # Update w
-        w = w - grad * gamma
-
-        # store w and loss
-        ws.append(w)
-        losses.append(loss)
-
+    for n_iter in range(max_iters): 
+        for by, btx in batch_iter(y,tx, batch_size):
+            grad, error = compute_gradient(y, tx, w)
+            loss = calculate_mse(error)
+            # ***************************************************
+            # INSERT YOUR CODE HERE
+            # TODO: update w by gradient
+            # ***************************************************
+            w = w - grad*gamma
+            # store w and loss
+            ws.append(w)
+            losses.append(loss)
     return losses[-1], ws[-1]
+
+def least_squares(y, tx):
+    """ Least squares using the normal equation """
+    w = np.linalg.solve(tx.T.dot(tx), tx.T.dot(y))
+    loss = compute_loss(y, tx, w)
+    
+    return loss, w
+
+def ridge_regression(y, tx, lambda_):
+    """implement ridge regression."""
+    aI = 2 * tx.shape[0] * lambda_ * np.identity(tx.shape[1])
+    a = tx.T.dot(tx) + aI
+    b = tx.T.dot(y)
+    w = np.linalg.solve(a, b)
+    loss = compute_loss(y,tx,w)
+    return loss, w
+
 
 
 if __name__ == "__main__":
     y_train, x_train, ids_train = load_csv_data("../data/train.csv")
     y_test, x_test, ids_test = load_csv_data("../data/test.csv")
-
+    
+    
     # Vi må behandle dataen vi har før vi bruker den
     # Visualiser data i en notebook, enklere der.. Kan ikke gjøre det nå pga. Anaconda er fucka..
+    
--- project1/scripts/proj1_helpers.py
+++ project1/scripts/proj1_helpers.py
@@ -46,3 +46,69 @@ def create_csv_submission(ids, y_pred, name):
         writer.writeheader()
         for r1, r2 in zip(ids, y_pred):
             writer.writerow({'Id':int(r1),'Prediction':int(r2)})
+            
+def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
+    """
+    Generate a minibatch iterator for a dataset.
+    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
+    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
+    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
+    Example of use :
+    for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
+        <DO-SOMETHING>
+    """
+    data_size = len(y)
+
+    if shuffle:
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        shuffled_y = y[shuffle_indices]
+        shuffled_tx = tx[shuffle_indices]
+    else:
+        shuffled_y = y
+        shuffled_tx = tx
+    for batch_num in range(num_batches):
+        start_index = batch_num * batch_size
+        end_index = min((batch_num + 1) * batch_size, data_size)
+        if start_index != end_index:
+            yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
+            
+def calculate_mse(e):
+    """Calculate the mse for vector e."""
+    N = len(e)
+
+    loss = 1/(2*N) * np.sum(e**2, axis=0)
+    return loss
+
+def compute_loss(y, tx, w):
+    return 1/(2*tx.shape[0]) * sum((y-tx.dot(w))**2)
+
+
+def compute_gradient(y, tx, w):
+    err = y - tx.dot(w)
+    grad = -tx.T.dot(err) / len(err)
+    return grad, err
+
+def build_poly(x, degree):
+    """polynomial basis functions for input data x, for j=0 up to j=degree."""
+    poly = np.ones((len(x), 1))
+    for deg in range(1, degree+1):
+        poly = np.c_[poly, np.power(x, deg)]
+    return poly
+
+def split_data(x, y, ratio, myseed=1):
+    """split the dataset based on the split ratio."""
+    # set seed
+    np.random.seed(myseed)
+    # generate random indices
+    num_row = len(y)
+    indices = np.random.permutation(num_row)
+    index_split = int(np.floor(ratio * num_row))
+    index_tr = indices[: index_split]
+    index_te = indices[index_split:]
+    # create split
+    x_tr = x[index_tr]
+    x_te = x[index_te]
+    y_tr = y[index_tr]
+    y_te = y[index_te]
+    return x_tr, y_tr, x_te, y_te
+
